f1823e072043763458da3dc1662cb550fb74ba27
tranduydan2004 <129383840+tranduydan2004@users.noreply.github.com>
2025-10-26 23:12:07 +0700

    Implement in Order service - Infrastructure layer: Preparing for DB connect and DB settings - Adding Data/ApplicationDbContext.cs - Adding Repositories/TransactionRepository.cs

---
+++ src/order-service/Order.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Order.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Order.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Order.Domain.Entities.Transaction> Transactions { get; set; }
+    }
+}
---
+++ src/order-service/Order.Infrastructure/Repositories/TransactionRepository.cs
@@ -0,0 +1,34 @@
+﻿using Order.Domain.Abstraction;
+using Order.Domain.Entities;
+using Order.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Order.Infrastructure.Repositories
+{
+    public class TransactionRepository : ITransactionRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+        
+        public TransactionRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Order.Domain.Entities.Transaction> GetByIdAsync(int id)
+        {
+            return await _dbContext.Transactions.FirstOrDefaultAsync(t => t.TransactionId == id);
+        }
+
+        public async Task AddAsync(Order.Domain.Entities.Transaction transaction)
+        {
+            await _dbContext.Transactions.AddAsync(transaction);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Order.Domain.Entities.Transaction transaction)
+        {
+            _dbContext.Transactions.Update(transaction);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
