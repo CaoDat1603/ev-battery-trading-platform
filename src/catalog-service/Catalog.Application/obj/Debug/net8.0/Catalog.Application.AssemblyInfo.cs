ef8e99135593aca02eec9356138bf15b70c2f53c
Tran Van Du <trandu7890@gmail.com>
2025-11-11 01:56:04 +0700

    Update kong.yml (add catalog route)

--- src/catalog-service/Catalog.Application/obj/Debug/net8.0/Catalog.Application.AssemblyInfo.cs
+++ src/catalog-service/Catalog.Application/obj/Debug/net8.0/Catalog.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Catalog.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+949ff9d3f8409b1a473660520c23c2d793ee15f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61ef125300a90542de6c2777edf7e9bf713dc01d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Catalog.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Catalog.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
